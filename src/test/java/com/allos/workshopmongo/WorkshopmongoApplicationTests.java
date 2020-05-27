63fc1916f2bb4e4d25b4bc90b74f37b061f74728
albertolopes <alberto_llopes@hotmail.com>
2020-05-27 12:58:45 -0300

    Criação do projeto

---
+++ src/test/java/com/allos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
